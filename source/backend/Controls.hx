2440b33e31500875c8a47ab9f606fda9197bdc64
8bit <139103755+RGBXD4@users.noreply.github.com>
2025-01-31 09:34:53 +0800

    Update Controls.hx

--- source/backend/Controls.hx
+++ source/backend/Controls.hx
@@ -30,6 +30,8 @@ class Controls
 	public var NOTE_DOWN_P(get, never):Bool;
 	public var NOTE_LEFT_P(get, never):Bool;
 	public var NOTE_RIGHT_P(get, never):Bool;
+	public var Q_P(get, never):Bool;
+        public var E_P(get, never):Bool;
 	private function get_UI_UP_P() return justPressed('ui_up');
 	private function get_UI_DOWN_P() return justPressed('ui_down');
 	private function get_UI_LEFT_P() return justPressed('ui_left');
@@ -38,6 +40,8 @@ class Controls
 	private function get_NOTE_DOWN_P() return justPressed('note_down');
 	private function get_NOTE_LEFT_P() return justPressed('note_left');
 	private function get_NOTE_RIGHT_P() return justPressed('note_right');
+        private function get_E_P() return justPressed('e');
+	private function get_Q_P() return justPressed('q');
 	
 	// Held buttons (directions)
 	public var UI_UP(get, never):Bool;
@@ -48,6 +52,8 @@ class Controls
 	public var NOTE_DOWN(get, never):Bool;
 	public var NOTE_LEFT(get, never):Bool;
 	public var NOTE_RIGHT(get, never):Bool;
+	public var Q(get, never):Bool;
+        public var E(get, never):Bool;
 	private function get_UI_UP() return pressed('ui_up');
 	private function get_UI_DOWN() return pressed('ui_down');
 	private function get_UI_LEFT() return pressed('ui_left');
@@ -56,6 +62,8 @@ class Controls
 	private function get_NOTE_DOWN() return pressed('note_down');
 	private function get_NOTE_LEFT() return pressed('note_left');
 	private function get_NOTE_RIGHT() return pressed('note_right');
+	private function get_E() return justPressed('e');
+	private function get_Q() return justPressed('q');
 
 
 	// Released buttons (directions)
@@ -67,6 +75,8 @@ class Controls
 	public var NOTE_DOWN_R(get, never):Bool;
 	public var NOTE_LEFT_R(get, never):Bool;
 	public var NOTE_RIGHT_R(get, never):Bool;
+	public var Q_R(get, never):Bool;
+        public var E_R(get, never):Bool;
 	private function get_UI_UP_R() return justReleased('ui_up');
 	private function get_UI_DOWN_R() return justReleased('ui_down');
 	private function get_UI_LEFT_R() return justReleased('ui_left');
@@ -75,6 +85,8 @@ class Controls
 	private function get_NOTE_DOWN_R() return justReleased('note_down');
 	private function get_NOTE_LEFT_R() return justReleased('note_left');
 	private function get_NOTE_RIGHT_R() return justReleased('note_right');
+	private function get_E_R() return justPressed('e');
+	private function get_Q_R() return justPressed('q');
 
 	// Pressed buttons (others)
 	public var ACCEPT(get, never):Bool;
